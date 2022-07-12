cead2d3ad38bb0e01cec3c7180ae5b4033127224
KhoiNguyen1998 <96235871+KhoiNguyen1998@users.noreply.github.com>
2022-07-12 11:12:27 +0700

    message: create file const.dart to set const for some variable

---
+++ lib/const/const.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+const String svg_account = 'assets/svg/Account.svg';
+const String svg_clock = 'assets/svg/clock.svg';
+const String svg_event = 'assets/svg/Event.svg';
+const String svg_home = 'assets/svg/Home.svg';
+const String svg_logo = 'assets/svg/logo.svg';
+const String svg_members = 'assets/svg/Members.svg';
+const String svg_message = 'assets/svg/Message.svg';
+const String svg_monorchy = 'assets/svg/monarchy.svg';
+const String svg_monarchy_gold = 'assets/svg/monarchy_gold.svg';
+const String svg_speaker = 'assets/svg/Speaker.svg';
+const String svg_search = 'assets/svg/Vector.svg';
+const String svg_back = 'assets/svg/back.svg';
+const String svg_setting = 'assets/svg/symbol.svg';
+
+/// Color
+const colorBottomIcon = const Color(0xFF212862);
+const backgroundButtombarColor = const Color(0xFFF1F1F1);
+const textBottombarColor = const Color(0xFF212862);
+const headerColor = const Color(0xFFF2F2F2);
+const searchBoxColor = const Color(0xFFEAE9E9);
+const textSearchColor = const Color(0xFF060606);
+
+/// String
+
+const String home = 'ホーム';
+const String myPage = 'マイページ';
+const String userAnnouncement = 'ユーザー告知';
+const String eventCommunity = 'イベント\nコミュニティ';
+const String listMember = '会員一覧';
+const String message = 'メッセージ';
+const String sortByConditions = '詳細条件を指定して検索する';
+const String showRecentlyLoggedUsers = '最近ログインしたユーザーを表示';
+const String showUsersRank = 'ユーザーランキングを表示';
